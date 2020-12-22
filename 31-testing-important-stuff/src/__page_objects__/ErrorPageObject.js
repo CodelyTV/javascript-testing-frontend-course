d0cfc5f52d9a5fe3f552d56141c6ec7625008e02
Nuria <soriano.nuria@gmail.com>
2020-12-22 17:07:34 +0100

    add lesson 3-1: testing the important stuff start

---
+++ 31-testing-important-stuff/src/__page_objects__/ErrorPageObject.js
@@ -0,0 +1,23 @@
+import { nextTick } from "vue";
+
+export default class ErrorPageObject {
+  constructor(wrapper) {
+    this.wrapper = wrapper;
+  }
+
+  text() {
+    return this.wrapper.text();
+  }
+
+  async wait() {
+    await nextTick();
+  }
+
+  hasErrorMessage() {
+    return this.wrapper.find('[data-test-id="message"]').exists();
+  }
+
+  clickToggleButton() {
+    this.wrapper.find("#toggle-message").trigger("click");
+  }
+}
---
+++ 31-testing-important-stuff/test/01-ErrorToggle.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from "@vue/test-utils";
+import ErrorToggle from "@/components/ErrorToggle.vue";
+
+describe("Error Toggle", () => {
+  it("should toggle open property when button is clicked", async () => {
+    const wrapper = shallowMount(ErrorToggle);
+    const button = wrapper.find("#toggle-message");
+    button.trigger("click");
+    expect(wrapper.vm.open).toBe(true);
+    button.trigger("click");
+    expect(wrapper.vm.open).toBe(false);
+  });
+});
