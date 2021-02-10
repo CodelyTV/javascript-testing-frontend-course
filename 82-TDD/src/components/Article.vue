744f5808a451b8cea5279614a9e54028d83f81cf
Nuria <soriano.nuria@gmail.com>
2021-02-10 15:55:34 +0100

    add lesson 8-1: add comments code

--- 82-TDD/src/components/Article.vue
+++ 82-TDD/src/components/Article.vue
@@ -1,16 +1,20 @@
 <template>
   <section class="article">
     <ArticleContent v-if="article" :article="article" />
+    <Comments v-if="comments" :comments="comments" />
   </section>
 </template>
 
 <script>
 import { getArticle } from "@/services/articlesRepository";
+import { getAllComments } from "@/services/commentsRepository";
 import ArticleContent from "@/components/ArticleContent";
+import Comments from "@/components/Comments";
 
 export default {
   components: {
     ArticleContent,
+    Comments,
   },
   props: {
     postId: {
@@ -21,10 +25,12 @@ export default {
   data() {
     return {
       article: null,
+      comments: null,
     };
   },
   async created() {
     this.article = await getArticle(this.postId);
+    this.comments = await getAllComments(this.postId);
   },
 };
 </script>
---
+++ 82-TDD/src/components/Comments.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="comments">
+    <h2>Comments</h2>
+    <Comment v-for="comment in comments" :key="comment.id" :comment="comment" />
+  </section>
+</template>
+
+<script>
+import Comment from "@/components/Comment.vue";
+
+export default {
+  components: {
+    Comment,
+  },
+  props: {
+    comments: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
